2938da105017a1a9c4bb7da50b3f438fd7066dcd
Yuan Fang <yfang6@uoregon.edu>
2019-05-05 21:36:27 -0700

    Wrote codes to load data into R.

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -1,10 +1,13 @@
 #' ---
 #' title: "organize_data.R"
-#' author: ""
+#' author: "Yuan Fang"
 #' ---
 
 # This script will read in raw data from the input directory, clean it up to produce 
-# the analytical dataset, and then write the analytical data to the output directory. 
+# the analytical dataset, and then write the analytical data to the output directory.
+
+# Loads data into R
+huizui <- read.csv("analysis/input/Huizui.csv")
 
 #source in any useful functions
 source("useful_functions.R")
